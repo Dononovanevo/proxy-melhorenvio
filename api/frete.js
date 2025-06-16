92eca93f5313437f9b80d60e8ef659412876965b
Dononovanevo <don.destravandocomia@gmail.com>
2025-06-16 14:27:34 -0300

    Update frete.js

--- api/frete.js
+++ api/frete.js
@@ -2,23 +2,22 @@ export default async function handler(req, res) {
   const path = req.url.replace('/api/frete', '');
   const url = `https://api.melhorenvio.com.br${path}`;
 
-  const headers = {
-    ...req.headers,
-    host: 'api.melhorenvio.com.br',
-  };
+  const headers = { ...req.headers };
+  delete headers.host;
 
   const options = {
     method: req.method,
     headers,
   };
 
-  if (!['GET', 'HEAD'].includes(req.method)) {
+  if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
     options.body = JSON.stringify(req.body);
   }
 
   try {
     const response = await fetch(url, options);
     const data = await response.text();
+
     res.status(response.status).send(data);
   } catch (err) {
     res.status(500).json({ error: true, message: err.message });
